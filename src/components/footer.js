735c10d97952f19331e27dc5a14abee77507e5c0
MATEUSZ PAWLOWSKI - STUDENT <G00361162@gmit.ie>
2019-09-26 14:32:08 +0100

    Second Commit

---
+++ src/components/footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../App.css';
+
+class Footer extends React.Component {
+  render(){
+    return (
+        <div className="App">
+            <h1>My Footer is another Component</h1>
+        </div>
+      );
+    }
+  }
+
+export default Footer;
