3ddec319483d3548ba90be134cedc767dc68db9e
MATEUSZ PAWLOWSKI - STUDENT <G00361162@gmit.ie>
2019-10-03 14:36:28 +0100

    lab_upload

---
+++ src/components/create.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Create extends React.Component {
+  render(){
+    return (
+        <div>
+            <h1>Hello from create Component</h1>
+        </div>
+      );
+    }
+  }
+
+export default Create;
