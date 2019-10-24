0b4e8ea3c13898319de6b50bbe0c214737d39335
MATEUSZ PAWLOWSKI - STUDENT <G00361162@gmit.ie>
2019-10-24 14:34:34 +0100

    Lab Uploaded

--- src/components/create.js
+++ src/components/create.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 
 class Create extends React.Component {
   constructor(props) {
@@ -27,6 +28,15 @@ class Create extends React.Component {
   handleSubmit(e) {
     alert(this.state.Title + "  " + this.state.Year + "  " + this.state.Poster);
     e.preventDefault();
+
+    const movieObject = {
+      title: this.state.Title,
+      year: this.state.Year,
+      poster: this.state.Poster
+    }
+    axios.post('http://localhost:4000/api/movies', movieObject)
+
+
     this.setState({
       Title: '',
       Year: '',
